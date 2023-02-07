3d549f3a401fb190edf2ec1c541abccc78ad8c9f
Nick Johnson <nlj222@gmail.com>
2023-02-06 17:16:43 -0800

    more review

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,11 +1,26 @@
 // import {useState} from 'react'
 
-function Filters({greasedChecked, onGreasedChange}){
+function Filters({greasedChecked, onGreasedChange, sortBy, onSortBy}){
 
     return (
         <form>
             <label htmlFor="greased">Greased </label>
             <input type="checkbox" id="greased" checked={greasedChecked} onChange={onGreasedChange} />
+            <fieldset onChange={onSortBy}>
+                <legend>Sort:</legend>
+                <div>
+                <input type="radio" id="all" value="all" checked={sortBy === "all"} />
+                <label for="all">All</label>
+                </div>
+                <div>
+                <input type="radio" id="name" value="name" checked={sortBy === "name"} />
+                <label for="name">Name</label>
+                </div>
+                <div>
+                <input type="radio" id="weight" value="weight" checked={sortBy === "weight"} />
+                <label for="weight">Weight</label>
+                </div>
+            </fieldset>
         </form>
     )
 }
--- src/components/HogsContainer.js
+++ src/components/HogsContainer.js
@@ -6,9 +6,15 @@ import HogTile from './HogTile'
 function HogsContainer(){
 
     const [greasedChecked, setGreasedChecked] = useState(false)
+    const [sortBy, setSortBy] = useState('all')
     
     function handleGreasedChange(e){
         setGreasedChecked(e.target.checked)
+    }
+    
+    function handleSortBy(e){
+        console.log(e.target.value);
+        setSortBy(e.target.value)
     }
 
     const filterGreasedHogs = hogs.filter(hog => {
@@ -18,13 +24,23 @@ function HogsContainer(){
         return true
     })
 
+    // function sortHogs(){
+    //     if(sortBy === 'all'){
+
+    //     } else if (sortBy === 'name'){
+    //         filteredGreasedHogs.sort((hog) => )
+    //     } else if (sortBy === 'weight'){
+
+    //     }
+    // }
+
     const hogTiles = filterGreasedHogs.map((hog)=>
         <HogTile key={hog.name} {...hog} />
     )
 
     return(
         <section>
-            <Filters greasedChecked={greasedChecked} onGreasedChange={handleGreasedChange}/>
+            <Filters greasedChecked={greasedChecked} onGreasedChange={handleGreasedChange} sortBy={sortBy} onSortBy={handleSortBy} />
             {hogTiles}
         </section>
     )
